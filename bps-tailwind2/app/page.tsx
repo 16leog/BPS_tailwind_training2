7490f39f1dd00d107255b3692795636ae8d490b6
Leonel Gonzalez <16leog@gmail.com>
2023-06-23 16:59:10 -0500

    Halfway done with chapter 4, unsure which route to take in implementing logic for hamburger menu

--- bps-tailwind2/app/page.tsx
+++ bps-tailwind2/app/page.tsx
@@ -14,15 +14,27 @@ export default function Home() {
             <a href="#hero">🚀 Acme Rockets</a>
           </h1>
           <div>
-            <button id="mobile-open-button" className="text-3xl sm:hidden focus:outline-none">
+            <button id="hamburger-button" className="text-3xl md:hidden cursor-pointer">
               &#9776;
             </button>
-            <nav className="hidden sm:block space-x-8 text-xl" aria-label="main">
+            <nav className="hidden md:block space-x-8 text-xl" aria-label="main">
               <a href="#rockets" className="hover:opacity-90">Our Rockets</a>
               <a href="#testimonials" className="hover:opacity-90">Testimonials</a>
               <a href="#contact" className="hover:opacity-90">Contact Us</a>
             </nav>
           </div>
+        </section>
+        <section id="mobile-menu" className='absolute top-0 bg-black w-full text-5xl flex-col justify-content-center origin-top animate-open-menu hidden'>
+          <button className="text-8xl self-end px-6">
+            &times;
+          </button>
+          <nav className="flex flex-col min-h-screen items-center py-8" aria-label='mobile'>
+            <a href="#hero" className='w-full text-center py-6 hover:opacity-90'>Home</a>
+            <a href="#rockets" className='w-full text-center py-6 hover:opacity-90'>Rockets</a>
+            <a href="#testimonials" className='w-full text-center py-6 hover:opacity-90'>Testimonials</a>
+            <a href="#contact" className='w-full text-center py-6 hover:opacity-90'>Contact Us</a>
+            <a href="#footer" className='w-full text-center py-6 hover:opacity-90'>Legal</a>
+          </nav>
         </section>
       </header>
       <main className="max-w-4xl mx-auto">
